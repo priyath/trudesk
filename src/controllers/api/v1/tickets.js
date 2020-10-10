d47e2f1c19aa43fb5efe7fa7bab0d9768bd83290
Priyath Gregory <priyath.gregory@syscolabs.com>
2020-10-10 20:25:21 +0530

    subsribe default assignee to ticket

--- src/controllers/api/v1/tickets.js
+++ src/controllers/api/v1/tickets.js
@@ -462,7 +462,7 @@ apiTickets.create = function (req, res) {
     tIssue = sanitizeHtml(tIssue).trim()
     ticket.issue = marked(tIssue)
     ticket.history = [HistoryItem]
-    ticket.subscribers = [req.user._id]
+    ticket.subscribers = defaultAssignee ? [req.user._id, defaultAssignee] : [req.user._id];
 
     if (defaultAssignee) {
       ticket.assignee = defaultAssignee;
